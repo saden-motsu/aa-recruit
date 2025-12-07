c9f25c919dea1b8dfb98265c7c4e06f01dc84e60
Saden Motsu <saden.motsu@gmail.com>
2025-12-07 00:11:32 -0500

    List users and their main characters.

--- recruit/tests/test_example.py
+++ recruit/tests/test_example.py
@@ -3,29 +3,87 @@ Recruit Test
 """
 
 # Django
+from django.contrib.auth.models import Permission, User
 from django.test import TestCase
+from django.urls import reverse
+
+# Alliance Auth
+from allianceauth.authentication.models import CharacterOwnership
+from allianceauth.eveonline.models import EveCharacter
 
 
-class TestRecruit(TestCase):
-    """
-    TestRecruit
-    """
+class RecruitIndexViewTests(TestCase):
+    """Tests for the Recruit index view."""
 
-    @classmethod
-    def setUpClass(cls) -> None:
-        """
-        Test setup
-        :return:
-        :rtype:
-        """
+    def setUp(self) -> None:
+        self.user = User.objects.create_user(username="main-user")
+        permission = Permission.objects.get(
+            codename="basic_access",
+            content_type__app_label="recruit",
+        )
+        self.user.user_permissions.add(permission)
+        self.client.force_login(self.user)
 
-        super().setUpClass()
+        main_character = self._create_character(character_id=1100, name="Main Pilot")
+        profile = self.user.profile
+        profile.main_character = main_character
+        profile.save(update_fields=["main_character"])
 
-    def test_recruit(self):
-        """
-        Dummy test function
-        :return:
-        :rtype:
-        """
+        alt_character = self._create_character(character_id=1200, name="Alt Pilot")
+        self.primary_ownership = CharacterOwnership.objects.create(
+            user=self.user,
+            character=alt_character,
+            owner_hash="owner-hash-1",
+        )
 
-        self.assertEqual(True, True)
+        other_user = User.objects.create_user(username="other-user")
+        other_main = self._create_character(character_id=2100, name="Other Main")
+        other_profile = other_user.profile
+        other_profile.main_character = other_main
+        other_profile.save(update_fields=["main_character"])
+
+        other_character = self._create_character(character_id=2200, name="Other Alt")
+        self.secondary_ownership = CharacterOwnership.objects.create(
+            user=other_user,
+            character=other_character,
+            owner_hash="owner-hash-2",
+        )
+
+    def _create_character(self, *, character_id: int, name: str) -> EveCharacter:
+        """Helper to create an EveCharacter with minimal required fields."""
+
+        return EveCharacter.objects.create(
+            character_id=character_id,
+            character_name=name,
+            corporation_id=character_id + 10,
+            corporation_name=f"Corporation {character_id}",
+            corporation_ticker=f"C{character_id}"[:5],
+            alliance_id=None,
+            alliance_name="",
+            alliance_ticker="",
+            faction_id=None,
+            faction_name="",
+        )
+
+    def test_index_lists_character_ownerships_labelled_by_main_character(self):
+        response = self.client.get(reverse("recruit:index"))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("character_ownerships", response.context)
+        self.assertEqual(
+            response.context["character_ownerships"],
+            [
+                {
+                    "id": str(self.primary_ownership.pk),
+                    "main_character_name": "Main Pilot",
+                    "character_name": "Alt Pilot",
+                },
+                {
+                    "id": str(self.secondary_ownership.pk),
+                    "main_character_name": "Other Main",
+                    "character_name": "Other Alt",
+                },
+            ],
+        )
+        self.assertContains(response, "Main Pilot - Alt Pilot")
+        self.assertContains(response, "Other Main - Other Alt")
--- recruit/views.py
+++ recruit/views.py
@@ -6,6 +6,21 @@ from django.core.handlers.wsgi import WSGIRequest
 from django.http import HttpResponse
 from django.shortcuts import render
 
+# Alliance Auth
+from allianceauth.authentication.models import UserProfile
+
+
+def _get_main_characters() -> list[(int, str)]:
+    """
+    Gets a list of all main character names.
+    """
+
+    return (
+        UserProfile.objects.exclude(main_character__isnull=True)
+        .order_by("user__date_joined")
+        .values_list("user__username", "main_character__character_name")
+    )
+
 
 @login_required
 @permission_required("recruit.basic_access")
@@ -16,6 +31,8 @@ def index(request: WSGIRequest) -> HttpResponse:
     :return:
     """
 
-    context = {"text": "Hello, World!"}
+    context = {
+        "main_characters": _get_main_characters(),
+    }
 
     return render(request, "recruit/index.html", context)
